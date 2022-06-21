d63417021cb7b09905ca5e57f21ca1fa7490fc52
Sampaio00 <106176285+Sampaio00@users.noreply.github.com>
2022-06-21 11:19:10 -0500

    atualização do codigo do cadastro - malu

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,19 @@
 <?php
+
+    include ('conexao.php');
+
+    $nome = $_REQUEST['name'];
+    $nascimento = $_REQUEST['date'];
+    $email = $_REQUEST['email'];
+    $senha = $_REQUEST['password'];
+
+    $sql = "INSERT INTO usuarios (`nome`, `nascimento`, `email`, `senha`)  VALUES ('$nome', '$nascimento', '$email', '$senha')";
     
-    include (conexao.php);
+    if (mysqli_query($conexao, $sql)) 
+        echo "Cadastro realizado com sucesso!";
+    else 
+        echo "Erro! ". mysqli_connect_error($conexao);
+    
+    mysqli_close($conexao);
 
 ?>
