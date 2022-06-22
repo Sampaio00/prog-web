65105f4d3c13d359770670f5e066e9f67ab1e076
Sampaio00 <106176285+Sampaio00@users.noreply.github.com>
2022-06-22 08:23:58 -0500

    atualização on

--- cadastro.php
+++ cadastro.php
@@ -1,19 +1,20 @@
 <?php
 
-    include ('conexao.php');
+//conexão com o banco de dados via include
+include("./conexao.php");
 
-    $nome = $_REQUEST['name'];
-    $nascimento = $_REQUEST['date'];
-    $email = $_REQUEST['email'];
-    $senha = $_REQUEST['password'];
+$nome = $_POST['name'];
+$nascimento = $_POST['date'];
+$email = $_POST['email'];
+$senha = $_POST['password'];
 
-    $sql = "INSERT INTO usuarios (`nome`, `nascimento`, `email`, `senha`)  VALUES ('$nome', '$nascimento', '$email', '$senha')";
-    
-    if (mysqli_query($conexao, $sql)) 
-        echo "Cadastro realizado com sucesso!";
-    else 
-        echo "Erro! ". mysqli_connect_error($conexao);
-    
-    mysqli_close($conexao);
+$sql = "INSERT INTO usuarios(`nome`, `nascimento`, `email`, `senha`) VALUES ('$nome', '$nascimento', '$email', '$senha')";
+
+if(mysqli_query($conexao, $sql))
+    echo "Cadastro realizado com sucesso!";
+else
+    echo "Erro! ".mysqli_connect_error($conexao);
+
+mysqli_close($conexao);
 
 ?>
